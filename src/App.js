a40a0e60656abb554a17e01ca7761a886638824d
EnrisKumi <107574553+EnrisKumi@users.noreply.github.com>
2022-07-19 20:37:35 +0200

    react commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+
+const App = () => {
+
+  const [counter,setCounter] = useState(0); 
+
+  useEffect(()=> {
+    alert('You have changes the counter to ' + counter);
+  }, [counter]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <button onClick={() => setCounter((prevCount)=> prevCount - 1)}>-</button>
+     <h1>{counter}</h1>
+     <button onClick={() => setCounter((prevCount)=> prevCount + 1)}>+</button>
+     
+     </div>
   );
 }
 
